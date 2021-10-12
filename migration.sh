2ab40657fb74e905775efb932a25f5b432f1a769
Giancarlo Mora <giank.ma@gmail.com>
2021-10-12 15:09:45 -0600

    minor changes

--- migration.sh
+++ migration.sh
@@ -2,7 +2,7 @@
 
 echo "Downloading and installing flyway..."
 wget -qO- https://repo1.maven.org/maven2/org/flywaydb/flyway-commandline/8.0.0/flyway-commandline-8.0.0-linux-x64.tar.gz | \
-    tar xvz && sudo ln -s `pwd`/flyway-8.0.0/flyway /usr/local/bin
+    tar xz && sudo ln -s `pwd`/flyway-8.0.0/flyway /usr/local/bin
 echo "flyway successfully installed"
 
 flyway --version
